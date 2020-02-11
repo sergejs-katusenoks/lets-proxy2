98d56807a42e0dd3d6344b8a5a2154446c20846b
Sergejs Katusenoks <sergejs.katusenoks@hellosoda.com>
2020-02-11 14:37:06 +0000

    perfect-ssl-labs-score

--- cmd/main.go
+++ cmd/main.go
@@ -113,9 +113,23 @@ func startProfiler(ctx context.Context, config profiler.Config) {
 	}
 
 	go func() {
+
+		cfg := &tls.Config{
+			MinVersion:               tls.VersionTLS12,
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+		}
+
 		httpServer := http.Server{
-			Addr:    config.BindAddress,
-			Handler: profiler.New(logger.Named("profiler"), config),
+			Addr:      config.BindAddress,
+			TLSConfig:  cfg,
+			Handler:   profiler.New(logger.Named("profiler"), config),
 		}
 
 		logger.Info("Start profiler", zap.String("bind_address", httpServer.Addr))
