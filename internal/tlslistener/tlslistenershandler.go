d257d4c1b1a804204160fcc9f54c8385a4e1c871
Sergejs Katusenoks <sergejs.katusenoks@hellosoda.com>
2020-02-13 10:52:23 +0000

    stronger ciphers

--- internal/tlslistener/tlslistenershandler.go
+++ internal/tlslistener/tlslistenershandler.go
@@ -133,8 +133,12 @@ func (p *ListenersHandler) init() {
 		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
+			tls.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_DHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 		},
